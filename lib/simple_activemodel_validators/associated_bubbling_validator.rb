7aaa5977b134f6ac85fc9b8b8c9ae730ed51dd27
Sahil Muthoo <sahil.muthoo@gmail.com>
2015-08-23 16:48:59 +0530

    Document some odd code

--- lib/simple_activemodel_validators/associated_bubbling_validator.rb
+++ lib/simple_activemodel_validators/associated_bubbling_validator.rb
@@ -1,6 +1,7 @@
 module SimpleActiveModelValidators
   class AssociatedBubblingValidator < ActiveModel::EachValidator
     def validate_each(record, attribute, values)
+      # values can be an array or a scalar
       [values].flatten.reject(&:valid?).each do |value|
         record.errors.add(attribute, value.errors.messages)
       end
